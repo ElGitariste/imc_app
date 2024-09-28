7453e6706c8235098d6ca7c930ae29c28ac73498
ElGitariste <43290960+ElGitariste@users.noreply.github.com>
2024-09-28 14:56:14 +0200

    js corrigé

--- script.js
+++ script.js
@@ -60,17 +60,8 @@ document.querySelector("#result").innerHTML = bmi.toFixed(2);
 
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 function countBmi(){
   var p = [age.value, height.value, weight.value];
   if(male.checked){
